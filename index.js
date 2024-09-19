369641d6eb90e102bb4948e9732464c011af03e8
atranimal <tran.andrew.a@gmail.com>
2024-09-19 15:24:44 -0400

    fix challenges iteration

--- index.js
+++ index.js
@@ -179,7 +179,9 @@ wss.on("connection", function (ws) {
           // }
 
           set(ref(db, `lobby/${opponent}`), {
-            challenges: [...playerInfo.challenges, playerInfo.playerName],
+            challenges: !!playerInfo.challenges
+              ? [playerInfo.playerName]
+              : [...playerInfo.challenges, playerInfo.playerName],
             ...opponentExistingLobby,
           });
         }
